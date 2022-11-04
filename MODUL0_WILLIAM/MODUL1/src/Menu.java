1c87c1b464ef7043cd03c4363a5498b3ca71d603
william <williamkm007@gmail.com>
2022-11-04 08:05:18 +0700

    MODUL1

---
+++ MODUL0_WILLIAM/MODUL1/src/Menu.java
@@ -0,0 +1,17 @@
+public class Menu {
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    String name;
+    String Category;
+    int price;
+
+    public void setname(String name) {
+        this.name = name;
+    }
+    public void setcategory(String category) {
+        this.Category = category;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
